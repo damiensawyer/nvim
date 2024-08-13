2082500ba9b59b409d82d67474b52945e68f584e
Damien Sawyer <damiensawyer@gmail.com>
2024-08-14 06:46:24 +1000

    undotree

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- Ensure the undo directory exists
+local undodir = vim.fn.expand("~/.config/nvim/undo")
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, "p")
+end
+
+-- Enable persistent undo
+opt.undofile = true
+opt.undodir = undodir
---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,15 @@
+return {
+  "mbbill/undotree",
+  lazy = false,
+  keys = {
+    {
+      "<leader>tu",
+      vim.cmd.UndotreeToggle,
+      desc = "Toggle undotree",
+    },
+  },
+}
+
+-- This is a comment.
+-- This is another comment
+-- This is really annoying that Codeium is puttin up those messages
