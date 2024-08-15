f83a4beccaa0cc7b98c59f8aa77d0f5c23b00823
Damien Sawyer <damiensawyer@gmail.com>
2024-08-15 18:29:32 +1000

    marks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,3 +13,17 @@ end
 -- Enable persistent undo
 opt.undofile = true
 opt.undodir = undodir
+
+-- Enable global marks across sessions
+vim.o.shada = [['100,<50,s10,h]]
+
+-- Allow hidden buffers to retain marks
+vim.o.hidden = true
+
+-- Function to delete all marks
+function DeleteAllMarks()
+  vim.cmd("delmarks! | delmarks a-zA-Z0-9")
+end
+
+-- Map 'mda' to delete all marks
+vim.api.nvim_set_keymap("n", "MD", ":lua DeleteAllMarks()<CR>", { noremap = true, silent = true })
--- lua/plugins/marks.lua
+++ lua/plugins/marks.lua
@@ -1,3 +1,6 @@
+if true then
+  return {}
+end
 return {
   {
     "chentoast/marks.nvim",
