112b2d2a1f5e8f8e4480a6ec3904cc263ac8674d
Damien Sawyer <damiensawyer@gmail.com>
2024-08-19 07:06:16 +1000

    cat

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,12 +1,12 @@
 return {
-  -- { "ellisonleao/gruvbox.nvim" },
-  { "lalitmee/cobalt2.nvim" },
+  { "ellisonleao/gruvbox.nvim" },
+  -- { "lalitmee/cobalt2.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "default",
+      colorscheme = "catppuccin-mocha",
     },
   },
 }
