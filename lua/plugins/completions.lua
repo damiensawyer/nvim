116aebd79101b5698a80adeef8daae39be717f12
Damien Sawyer <damiensawyer@gmail.com>
2024-08-19 12:03:31 +1000

    comp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,21 +1,31 @@
--- Ensure nvim-cmp is required
-local cmp = require("cmp")
+-- ~/.config/nvim/lua/plugins/completions.lua
 
--- Setup nvim-cmp
-cmp.setup({
-  completion = {
-    autocomplete = false, -- Disable automatic completion. The menu will only pop up when you press control space.
+return {
+  -- Specify the plugin and its configuration
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      -- Ensure nvim-cmp is required
+      local cmp = require("cmp")
+
+      -- Setup nvim-cmp
+      cmp.setup({
+        completion = {
+          autocomplete = false, -- Disable automatic completion. The menu will only pop up when you press Ctrl-Space.
+        },
+        mapping = {
+          ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion manually with Ctrl-Space
+          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
+          ["<Tab>"] = cmp.mapping.select_next_item(), -- Navigate to the next item
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- Navigate to the previous item
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
   },
-  mapping = {
-    ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion manually with Ctrl+Space
-    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
-    ["<Tab>"] = cmp.mapping.select_next_item(), -- Navigate to the next item
-    ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- Navigate to the previous item
-  },
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-  }, {
-    { name = "buffer" },
-  }),
-})
+}
