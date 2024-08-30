5905334ec8c93bc73806691b1a47f8a45087df72
Damien Sawyer <damiensawyer@gmail.com>
2024-08-30 13:08:04 +1000

    monorepo

--- lua/plugins/tscnvim.lua
+++ lua/plugins/tscnvim.lua
@@ -4,6 +4,7 @@ return {
     "dmmulroy/tsc.nvim",
     config = function()
       require("tsc").setup({
+        run_as_monorepo = true,
         -- Add any setup configuration here if needed
       })
     end,
